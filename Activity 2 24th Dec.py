9ff9f87a1ae24f6c4a35f6c2e2694ddf819c2901
kipurymalcolm-cmd <kipurymalcolm@gmail.com>
2025-12-24 08:47:30 +0300

    Add files via upload

---
+++ Activity 2 24th Dec.py
@@ -0,0 +1,10 @@
+#creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+#user input
+name = input("What is your name?")
+print("\nHello", name, "\nWelcome to codingal")
