0fb11725e7af072531e30e32268b18dfd9ce7532
kipurymalcolm-cmd <kipurymalcolm@gmail.com>
2026-01-07 14:22:46 +0300

    Add files via upload

---
+++ Activity 3 7th Jan.py
@@ -0,0 +1,9 @@
+print("Enter Marks Obtained In 4 Subjects:")
+Math=int(input("Math:"))
+English=int(input("English:"))
+Science=int(input("Science:"))
+Hindi=int(input("Hindi:"))
+sum=Math+English+Science+Hindi
+print("Total Marks Scored:",sum)
+perc=(sum/400)*100
+print(perc)
