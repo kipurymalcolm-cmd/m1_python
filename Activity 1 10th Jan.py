4e8fd486ff28bbbf9c6eac4db83780302273b173
kipurymalcolm-cmd <kipurymalcolm@gmail.com>
2026-01-10 16:50:18 +0300

    Add files via upload

---
+++ Activity 1 10th Jan.py
@@ -0,0 +1,13 @@
+v=3
+w=4
+x=5
+y=10
+z=v+w*x/y
+print("Value of v+w(x*y)is: ",z)
+
+name="Alex"
+age=0
+if name=="Alex" or name=="John" and age>=0:
+    print("Hello, welcome!")
+else:
+    print("Good Bye!")
