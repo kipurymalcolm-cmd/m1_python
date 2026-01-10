c4538ba00c1a4a96c28956becda695f0da824495
kipurymalcolm-cmd <kipurymalcolm@gmail.com>
2026-01-10 16:51:23 +0300

    Add files via upload

---
+++ Activity 3 10th Jan.py
@@ -0,0 +1,10 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+sum=mean1+total_number
+print("The sum of 40 numbers is: ",sum)
+sum1=sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number)):",sum1)
+mean2=sum1/total_number
+print(mean2)
