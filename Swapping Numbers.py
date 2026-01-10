8e923ed35791495b51aecb7aac4430f7bcec4fb9
kipurymalcolm-cmd <kipurymalcolm@gmail.com>
2026-01-10 17:10:50 +0300

    Add files via upload

---
+++ Swapping Numbers.py
@@ -0,0 +1,10 @@
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+c = int(input("Enter Third Number: "))
+
+a, b, c = c, a, b
+
+print("After Swapping:")
+print("First Number:", a)
+print("Second Number:", b)
+print("Third Number:", c)
