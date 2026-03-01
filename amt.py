4b0a7d1863dcae6440a374c5f44a49fdf12f2f57
kipurymalcolm-cmd <kipurymalcolm@gmail.com>
2026-03-01 12:17:59 +0300

    Add files via upload

---
+++ amt.py
@@ -0,0 +1,7 @@
+amount=int(input)("pleas enter amount for withdrall:")
+n1=amount//100
+n2=(amount%100)//50
+n3=((amount%100)%50)//10
+print("notes of 100 rupee is",n1)
+print("notes of 50 rupee is",n2)
+print("notes of 10 rupee is",n3)
